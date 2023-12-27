b99a05e4fdd161f713a7208cc642ca3fc930e075
Bohdan Kolomiiets <kb907406@gmail.com>
2023-12-27 18:22:32 +0200

    Implement Route & Station models

--- station/models.py
+++ station/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class Station(models.Model):
+    name = models.CharField(max_length=255)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Route(models.Model):
+    source = models.ForeignKey(
+        to=Station,
+        on_delete=models.CASCADE,
+        related_name="route_sources"
+    )
+    destination = models.ForeignKey(
+        to=Station,
+        on_delete=models.CASCADE,
+        related_name="route_destinations"
+    )
+    distance = models.IntegerField()
+
+    def __str__(self) -> str:
+        return f"{self.source} -> {self.destination}"
