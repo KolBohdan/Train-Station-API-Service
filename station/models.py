97e6d2b9ba4a750c7fea8994c6023ba4b6a460a1
Bohdan Kolomiiets <kb907406@gmail.com>
2023-12-27 18:23:17 +0200

    Add Crew & TrainType & Train models

--- station/models.py
+++ station/models.py
@@ -25,3 +25,36 @@ class Route(models.Model):
 
     def __str__(self) -> str:
         return f"{self.source} -> {self.destination}"
+
+
+class Crew(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return f"{self.first_name} {self.last_name}"
+
+
+class TrainType(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Train(models.Model):
+    name = models.CharField(max_length=255)
+    cargo_num = models.IntegerField()
+    places_in_cargo = models.IntegerField()
+    train_type = models.ForeignKey(
+        to=TrainType,
+        on_delete=models.CASCADE,
+        related_name="trains"
+    )
+
+    @property
+    def capacity(self) -> int:
+        return self.cargo_num * self.places_in_cargo
+
+    def __str__(self) -> str:
+        return self.name
