f499276469783afc164a49355c57d0e7700c368d
Bohdan Kolomiiets <kb907406@gmail.com>
2023-12-27 18:34:17 +0200

    Add Train and TrainType viewsets

--- station/views.py
+++ station/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework import mixins, viewsets
 
-# Create your views here.
+from train_station.models import (
+    TrainType,
+    Train,
+)
+from train_station.serializers import (
+    TrainTypeSerializer,
+    TrainSerializer,
+)
+
+
+class TrainTypeViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    viewsets.GenericViewSet,
+):
+    queryset = TrainType.objects.all()
+    serializer_class = TrainTypeSerializer
+
+
+class TrainViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    viewsets.GenericViewSet,
+):
+    queryset = Train.objects.all()
+    serializer_class = TrainSerializer
