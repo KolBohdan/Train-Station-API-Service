75f632df6c6b9bff25b16c13bec5e090540345fd
Bohdan Kolomiiets <kb907406@gmail.com>
2024-01-10 15:57:50 +0200

    Router register Crew, Station, Route models

--- station/urls.py
+++ station/urls.py
@@ -3,11 +3,17 @@ from rest_framework import routers
 from station.views import (
     TrainTypeViewSet,
     TrainViewSet,
+    CrewViewSet,
+    StationViewSet,
+    RouteViewSet,
 )
 
 router = routers.DefaultRouter()
 router.register("train_types", TrainTypeViewSet)
 router.register("trains", TrainViewSet)
+router.register("crews", CrewViewSet)
+router.register("stations", StationViewSet)
+router.register("routes", RouteViewSet)
 
 urlpatterns = router.urls
 
