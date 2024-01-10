9529154e9693bfe38e23bf3c084767f79a63c336
Bohdan Kolomiiets <kb907406@gmail.com>
2024-01-10 15:57:17 +0200

    Implement Viewsets for Crew, Station, Route models

--- station/views.py
+++ station/views.py
@@ -3,10 +3,16 @@ from rest_framework import mixins, viewsets
 from station.models import (
     TrainType,
     Train,
+    Crew,
+    Station,
+    Route,
 )
 from station.serializers import (
     TrainTypeSerializer,
     TrainSerializer,
+    CrewSerializer,
+    StationSerializer,
+    RouteSerializer,
 )
 
 
@@ -26,3 +32,30 @@ class TrainViewSet(
 ):
     queryset = Train.objects.all()
     serializer_class = TrainSerializer
+
+
+class CrewViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    viewsets.GenericViewSet,
+):
+    queryset = Crew.objects.all()
+    serializer_class = CrewSerializer
+
+
+class StationViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    viewsets.GenericViewSet,
+):
+    queryset = Station.objects.all()
+    serializer_class = StationSerializer
+
+
+class RouteViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    viewsets.GenericViewSet,
+):
+    queryset = Route.objects.all()
+    serializer_class = RouteSerializer
