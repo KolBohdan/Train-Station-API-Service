70acd50dd1bb2072853088668e4b6facc0b531f5
Bohdan Kolomiiets <kb907406@gmail.com>
2024-01-11 17:55:46 +0200

    Implement OrderSerializer & OrderListSerializer

--- station/serializers.py
+++ station/serializers.py
@@ -1,3 +1,4 @@
+from django.db import transaction
 from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
 
@@ -8,7 +9,7 @@ from station.models import (
     Station,
     Route,
     Journey,
-    Ticket,
+    Ticket, Order,
 )
 
 
@@ -145,3 +146,23 @@ class JourneyDetailSerializer(JourneySerializer):
             "crew",
             "taken_places"
         )
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    tickets = TicketSerializer(many=True, read_only=False, allow_empty=False)
+
+    class Meta:
+        model = Order
+        fields = ("id", "tickets", "created_at")
+
+    def create(self, validated_data):
+        with transaction.atomic():
+            tickets_data = validated_data.pop("tickets")
+            order = Order.objects.create(**validated_data)
+            for ticket_data in tickets_data:
+                Ticket.objects.create(order=order, **ticket_data)
+            return order
+
+
+class OrderListSerializer(OrderSerializer):
+    tickets = TicketListSerializer(many=True, read_only=True)
